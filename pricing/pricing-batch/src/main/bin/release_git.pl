63d48cc639117020909fdcd20de4a711fa1fafac
muthaiahs <Subramanian.Muthaiah@cgi.com>
2016-05-12 22:00:05 +0530

    Updated release script

--- pricing/pricing-batch/src/main/bin/release_git.pl
+++ pricing/pricing-batch/src/main/bin/release_git.pl
@@ -12,13 +12,15 @@ my $scriptDir = getcwd;
 print "scriptDir  :     $scriptDir  \n"; 
 
 my $url="https://github.com/muthaiahs/pricing.git";
-my $dir="E:/pricing_git/pricing";
+my $clone_dir="E:/pricing_git/pricing";
+
+my $local_repo="pricing";
 
 
 # my $git = Git::Repository->new(work_tree => "$dir",);
 
 # or init our own repository first
-Git::Repository->run( init => $dir );
+# Git::Repository->run( init => $dir );
 # my $r = Git::Repository->new( work_tree => $dir );
  
 # or clone from a URL first
@@ -27,10 +29,54 @@ Git::Repository->run( init => $dir );
 # my $local_dir = Git::Repository->run( clone => $url => $dir )  or die " Exiting:$!\n\n";
 # my $r = Git::Repository->new( work_tree => $dir );
 
+
+my $pricing_dir ="${scriptDir}/${local_repo}/${local_repo}";
+print " pricing_dir   :       $pricing_dir\n";  
+my $cmd=" clean install ";   
+my $args =" -X -e -U ";
+run_mvn_cmd("${pricing_dir}", "${cmd}" , "${args}");
+
+
 exit;  
 
 
   
+sub run_mvn_cmd() {
+ 
+	my $dir = shift;
+	my $cmd = shift;	
+	my $args = shift;
+	# check if dir exists 
+	chomp $dir;
+	chomp $cmd;
+	chomp $args;
+	
+	print "$dir \n  $cmd \n  $args  \n";  
+	
+	if(! -d $dir) {
+		die "\n    Invalid directory $dir  Exiting:$!   \n\n";
+    }		
+	
+	
+	my $MAVEN_HOME ="";	
+		
+	if ($ENV{"MAVEN_HOME"} eq "") {
+		$MAVEN_HOME="E:/apache-maven-3.3.9-bin/apache-maven-3.3.9";
+		print "Pricing release :  env field MAVEN_HOME is not set ,  Using path $MAVEN_HOME  \n"; 		
+	} else {
+		$MAVEN_HOME=$ENV{"MAVEN_HOME"};
+	}
+	
+    my @mvn_cmd = ("${MAVEN_HOME}/bin/mvn");
+    push @mvn_cmd, " ${cmd} ";
+    push @mvn_cmd, " ${args} ";
+    push @mvn_cmd, " -f ${dir}/pom.xml ";	
+	
+#	my $mvn_cmd = "${MAVEN_HOME}/bin/mvn ${cmd} ${args}";
+	print "Executing command:\n@mvn_cmd\n";			
+	system("@mvn_cmd");
+	
+}
 
 
 
--- pricing/pricing-batch/src/main/bin/test.pl
+++ pricing/pricing-batch/src/main/bin/test.pl
@@ -9,6 +9,7 @@ use Path::Class;
 use Cwd;
 use FileHandle;
 use English;
+use Git::Repository;
 
 # chdir to the script directory
 my $thisScript = $0;
@@ -21,41 +22,8 @@ my $dir="E:/pricing_git/pricing";
 my $user="muthaiahs";
 my $repo="pricing";
 
-
-my $p = Pithub->new;
-# my $p = Pithub->new(utf8 => 0); # enable compatibility options for version 0.01029 or lower
-my $result = $p->repos->get( user => 'muthaiahs', repo => 'pricing' );
-
-# $result->content is either an arrayref or an hashref
-# depending on the API call that has been made
-printf "%s\n", $result->content->{html_url};     # prints https://github.com/plu/Pithub
-printf "%s\n", $result->content->{clone_url};    # prints https://github.com/plu/Pithub.git
-
-# if the result is an arrayref, you can use the result iterator
-my $result = $p->repos->list( user => 'muthaiahs' );
-while ( my $row = $result->next ) {
-    printf "%s\n", $row->{name};
-}
-
-
-$p = Pithub->new(
-    api_uri => 'https://github.com/muthaiahs/pricing.git'
-);
-
-print "test   :    \n";  
-
-
-my $directory = 'test_git_1';
-dir($directory)->rmtree;
-
-# my $git = Git::PurePerl->init( directory => $directory );
-
-# $git->clone( 'https://github.com/muthaiahs/pricing.git' , "pricing" );
-my $git= Git::PurePerl ->clone( 'github.com', '/muthaiahs/pricing.git' )  or die "Error :  $! \n";
-# $git->get_object($git->master->tree);
-
- 
- 
+Git::Repository->run( clone => $url => $dir );
+ $r = Git::Repository->new( work_tree => $dir );
 
 
 
